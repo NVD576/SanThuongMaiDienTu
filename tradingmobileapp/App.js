622a301c4251452c981850467cce31747f448c72
Cyter <tu2004latoi@gmail.com>
2025-01-23 21:11:43 +0700

    tu dua len

--- tradingmobileapp/App.js
+++ tradingmobileapp/App.js
@@ -4,21 +4,7 @@ import Home from './components/Home/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Tú đầu buồi</Text>
-    </View>
+    <Home/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, // Chiếm toàn bộ màn hình
-    justifyContent: 'center', // Căn giữa theo trục dọc
-    alignItems: 'center', // Căn giữa theo trục ngang
-    backgroundColor: '#f0f0f0', // Màu nền (tuỳ chọn)
-  },
-  text: {
-    fontSize: 18, // Kích thước chữ (tuỳ chọn)
-    color: '#333', // Màu chữ (tuỳ chọn)
-  },
-});
