87cc19be5320ff8ac75a1b35de20d8a39a3e36e6
Cyter <tu2004latoi@gmail.com>
2025-01-23 20:49:46 +0700

    tu dua len

--- Trading/etradings/tradings/views.py
+++ Trading/etradings/tradings/views.py
@@ -8,8 +8,8 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.parsers import MultiPartParser
 
-from .models import Store, User, Product, Category, Review
-from .serializers import StoreSerializer, UserSerializer, ProductSerializer, CategorySerializer, ReviewSerializer
+from .models import Store, User, Product, Category, Review, Transaction, Order, Chat, OrderItem
+from .serializers import StoreSerializer, UserSerializer, ProductSerializer, CategorySerializer, ReviewSerializer, TransactionSerializer, OrderSerializer, OrderItemSerializer, ChatSerializer
 
 class UserViewSet(viewsets.ModelViewSet, generics.CreateAPIView, generics.RetrieveAPIView):
     queryset = User.objects.filter(is_active=True)
@@ -26,7 +26,7 @@ class StoresViewSet(viewsets.ModelViewSet):
     # queryset = Store.objects.filter(active=True)
     queryset = Store.objects.filter(active=True).order_by('id')
     serializer_class = StoreSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     # def get_permissions(self):
     #     if self.action=='list':
@@ -37,18 +37,18 @@ class StoresViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.filter(active=True).order_by('id')
     serializer_class = CategorySerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #
+    #     return [permissions.IsAuthenticated()]
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.filter(active=True).order_by('id')
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     @action(methods=['post'], detail=True, url_path="hide-product", url_name="hide-product")
     def hide_product(self, request, pk):
@@ -62,64 +62,64 @@ class ProductViewSet(viewsets.ModelViewSet):
         return Response(data=ProductSerializer(p, context={'request': request}).data,status=HTTP_200_OK)
 
 
-    def get_permissions(self):
-        if self.action == 'list':
-            return [permissions.AllowAny()]
-
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action == 'list':
+    #         return [permissions.AllowAny()]
+    #
+    #     return [permissions.IsAuthenticated()]
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.filter().order_by('id')
     serializer_class = ReviewSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #
+    #     return [permissions.IsAuthenticated()]
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.filter().order_by('id')
     serializer_class = OrderSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #
+    #     return [permissions.IsAuthenticated()]
 
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.filter().order_by('id')
     serializer_class = OrderItemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #
+    #     return [permissions.IsAuthenticated()]
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.filter().order_by('id')
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #     return [permissions.IsAuthenticated()]
 
 class ChatViewSet(viewsets.ModelViewSet):
     queryset = Chat.objects.filter().order_by('id')
     serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
-    def get_permissions(self):
-        if self.action=='list':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+    # def get_permissions(self):
+    #     if self.action=='list':
+    #         return [permissions.AllowAny()]
+    #     return [permissions.IsAuthenticated()]
--- tradingmobileapp/configs/APIs.js
+++ tradingmobileapp/configs/APIs.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = 'http://127.0.0.1:8000/';
+const BASE_URL = 'http://192.168.1.7:8000/';
 
 export const endpoints = {
-    'stores':'stores/',
-    'products':'products/'
+    'stores':'/stores/',
+    'products':'/products/',
+    'categorys':'/categorys/'
 }
 
 export default axios.create({
