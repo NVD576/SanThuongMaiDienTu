b8142b8474369cd5dc652da9af9d71b3b00c2434
Cyter <tu2004latoi@gmail.com>
2025-01-25 20:29:34 +0700

    tu dua len

--- tradingmobileapp/configs/UserReducers.js
+++ tradingmobileapp/configs/UserReducers.js
@@ -1,6 +1,3 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { authApis, endpoints } from "./APIs"
-
 const MyUserReducer = (currentState, action) => {
     switch (action.type) {
         case "login":
